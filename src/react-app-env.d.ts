b1647dca15f59775217ed5becf0102f101f43f99
Jordan Urbaez Lu <jordana.urbaez@gmail.com>
2022-09-06 17:42:46 -0400

    what is react-app-env.d.ts used for?

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="react-scripts" />
+
+/// WHAT DOES THIS FILE DO???
+
+/// <reference types="..." /> directive declares a dependency on a package.
+
+// The process of resolving these package names is similar to the process of resolving module names in an import statement. An easy way to think of these triple-slash-reference-types directives is as an import for declaration packages.
+
+// For example, including /// <reference types="node" /> in a declaration file declares that this file uses names declared in @types/node/index.d.ts and thus, this package needs to be included in the compilation along with the declaration file.
+
+// Use these directives only when you’re authoring a d.ts file by hand. More information: https://www.typescriptlang.org/docs/handbook/triple-slash-directives.html
+
+// In your case, /// <reference types="react-scripts" /> imports various names declared in the node_modules/react-scripts/lib/react-app.d.ts file.
