bea56ac1cbc1e38cccbf95c019a0700debc8735a
Jordan Urbaez Lu <jordana.urbaez@gmail.com>
2022-09-11 22:48:53 -0400

    added template for AstroCheckbox component

---
+++ src/components/AstroCheckbox/AstroCheckbox.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+
+export type AstroCheckboxVariant = "primary" | "secondary";
+
+export type AstroCheckboxSize = "large" | "medium" | "small";
+
+export type AstroCheckboxLabelPosition = "top" | "bottom" | "left" | "right";
+
+interface AstroCheckbox {
+    variant?: AstroCheckboxVariant;
+    disabled?: boolean;
+    size?: AstroCheckboxSize;
+    label?: string;
+    labelPosition?: AstroCheckboxLabelPosition;
+    checked?: boolean;
+}
+
+const AstroCheckbox: React.FC<AstroCheckbox> = ({
+
+}) => {
+    return <div></div>
+}
+
+export default AstroCheckbox;
