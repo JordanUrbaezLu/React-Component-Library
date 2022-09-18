8d7d87c79d517510bb16461f42ad767976798d29
Jordan Urbaez Lu <jordana.urbaez@gmail.com>
2022-09-18 12:24:51 -0400

    feat(deployment): add continuous deployment to github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,9 @@
 import styles from "./App.module.scss";
-import AstroSelect from "./components/AstroSelect/AstroSelect";
-import AstroSelectItem from "./components/AstroSelect/AstroSelectItem/AstroSelectItem";
 
 const App = () => {
   return (
     <div className={styles.AppContainer}>
-      <AstroSelect variant="primary" size="large">
-        <AstroSelectItem value="Lebron" />
-        <AstroSelectItem value="Three Pointer" />
-        <AstroSelectItem value="Toastre" />
-      </AstroSelect>
-      <AstroSelect variant="primary" size="medium" label="AstroSelect">
-        <AstroSelectItem value="Lebron" />
-        <AstroSelectItem value="Three Pointer" />
-        <AstroSelectItem value="Toastre" />
-      </AstroSelect>
-      <AstroSelect variant="primary" size="small" width={300}>
-        <AstroSelectItem value="Lebron" />
-        <AstroSelectItem value="Three Pointer" />
-        <AstroSelectItem value="Toastre" />
-      </AstroSelect>
+      <h1>Hello World!</h1>
     </div>
   );
 };
