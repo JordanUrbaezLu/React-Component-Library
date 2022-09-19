731fc9e61cbc7c7448a5d4a18f26aba93c7412ae
Jordan Urbaez Lu <jordana.urbaez@gmail.com>
2022-09-18 21:03:43 -0400

    feat(storybook): update sb

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,6 +6,7 @@ module.exports = {
     "@storybook/addon-interactions",
     "@storybook/preset-create-react-app",
     "@storybook/addon-a11y",
+    "storybook-dark-mode",
   ],
   framework: "@storybook/react",
   core: {
